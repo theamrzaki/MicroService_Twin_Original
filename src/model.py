3bc6210553ff54cb4b93ec3f3c63bc95651d6a81
amrzaki <theamrzaki@hotmail.com>
2025-07-13 10:46:44 -0400

    (LPF and Pai filters)

--- src/model.py
+++ src/model.py
@@ -4,6 +4,7 @@ from torch_geometric.utils import dense_to_sparse
 from src.model_util import *
 from src.inner_models.FITS import Model as FITSModel 
 from src.inner_models.FITS_LPF import Model as FITSModel_LPF 
+from src.inner_models.FITS_Pai import Model as FITSModel_Pai
 from src.inner_models.FourierGNN import FGN
 from src.inner_models.GPT4TS import Model as GPT2Model
 import argparse
@@ -36,7 +37,7 @@ class MyModel(nn.Module):
 							node_heads=args['num_heads_node'], log_heads=args['num_heads_log'], edge_heads=args['num_heads_edge'],
 							n2e_heads=args['num_heads_n2e'], e2n_heads=args['num_heads_e2n'],
 							dropout=args['dropout'], batch_size=args['batch_size'], window_size=args['window'], num_layer=args['num_layer'], trace2pod=trace2pod)
-		elif self.FREQ_DOMAIN == "FITS" or self.FREQ_DOMAIN == "FITS_LPF":
+		elif self.FREQ_DOMAIN in ["FITS_Pai","FITS_LPF","FITS"]:
 			class Config: pass
 			config = Config()
 
@@ -57,27 +58,35 @@ class MyModel(nn.Module):
 				config.enc_in = args['feature_node']
 				if self.FREQ_DOMAIN == "FITS":
 					self.fits_node = FITSModel(configs=config)
-				if self.FREQ_DOMAIN == "FITS_LPF":
+				elif self.FREQ_DOMAIN == "FITS_LPF":
 					self.fits_node = FITSModel_LPF(configs=config)
+				elif self.FREQ_DOMAIN == "FITS_Pai":
+					self.fits_node = FITSModel_Pai(configs=config)
 
 				config.enc_in = args['feature_log'] 
 				if self.FREQ_DOMAIN == "FITS":
 					self.fits_log = FITSModel(configs=config)
-				if self.FREQ_DOMAIN == "FITS_LPF":
+				elif self.FREQ_DOMAIN == "FITS_LPF":
 					self.fits_log = FITSModel_LPF(configs=config)
+				elif self.FREQ_DOMAIN == "FITS_Pai":
+					self.fits_log = FITSModel_Pai(configs=config)
 
 				config.enc_in = args['feature_edge'] 
 				if self.FREQ_DOMAIN == "FITS":
 					self.fits_edge = FITSModel(configs=config)
-				if self.FREQ_DOMAIN == "FITS_LPF":
+				elif self.FREQ_DOMAIN == "FITS_LPF":
 					self.fits_edge = FITSModel_LPF(configs=config)
+				elif self.FREQ_DOMAIN == "FITS_Pai":
+					self.fits_edge = FITSModel_Pai(configs=config)
 
 			else:
 				config.enc_in = 10
 				if self.FREQ_DOMAIN == "FITS":
 					self.shared_fits = FITSModel(configs=config)
-				if self.FREQ_DOMAIN == "FITS_LPF":
+				elif self.FREQ_DOMAIN == "FITS_LPF":
 					self.shared_fits = FITSModel_LPF(configs=config)
+				elif self.FREQ_DOMAIN == "FITS_Pai":
+					self.shared_fits = FITSModel_Pai(configs=config)
 				self.modality_proj = nn.ModuleDict({
 					'node': nn.Linear(args['feature_node'], config.enc_in),
 					'log': nn.Linear(args['feature_log'], config.enc_in),
@@ -202,7 +211,7 @@ class MyModel(nn.Module):
 			rec_edge = torch.matmul(rec_edge1.permute(
 				0, 1, 3, 2), self.trace2pod.float()).permute(0, 1, 3, 2)
 			rec = torch.concat([rec_node, rec_log, rec_edge], dim=-1)
-		elif self.FREQ_DOMAIN in ["FITS_LPF","FITS","GPT2"]:
+		elif self.FREQ_DOMAIN in ["FITS_Pai","FITS_LPF","FITS","GPT2"]:
 			B, T, _,_ = x['data_node'].shape
 			# get edge mask
 			edge_exists_mask = (self.node_efea.sum(dim=-1) != 0)  # [N, N] boolean mask
@@ -230,7 +239,9 @@ class MyModel(nn.Module):
 				if self.FREQ_DOMAIN == "FITS":
 					inner_model = self.shared_fits
 				elif self.FREQ_DOMAIN == "FITS_LPF":
-					inner_model = self.shared_fits_LPF
+					inner_model = self.shared_fits
+				elif self.FREQ_DOMAIN == "FITS_Pai":
+					inner_model = self.shared_fits
 				elif self.FREQ_DOMAIN == "GPT2":
 					inner_model = self.shared_GPT2
 				x_node_proj = self.modality_proj['node'](x_node_metric_fits_input)
