ee3e342fd60fa3213ad4ac617b1cc19bbb0c6eb3
amrzaki <theamrzaki@hotmail.com>
2025-07-16 12:52:51 -0400

    (FreTS neruIPS23)

--- src/model.py
+++ src/model.py
@@ -9,6 +9,7 @@ from src.inner_models.iTransformer import Model as iTransformerModel
 from src.inner_models.DLinear import Model as DLinearModel
 from src.inner_models.FourierGNN import FGN
 from src.inner_models.GPT4TS import Model as GPT2Model
+from src.inner_models.FreTS import Model as FreTSModel
 import argparse
 
 class MyModel(nn.Module):
@@ -40,7 +41,7 @@ class MyModel(nn.Module):
 							node_heads=args['num_heads_node'], log_heads=args['num_heads_log'], edge_heads=args['num_heads_edge'],
 							n2e_heads=args['num_heads_n2e'], e2n_heads=args['num_heads_e2n'],
 							dropout=args['dropout'], batch_size=args['batch_size'], window_size=args['window'], num_layer=args['num_layer'], trace2pod=trace2pod)
-		elif self.FREQ_DOMAIN in ["FITS_Pai","FITS_LPF","FITS","iTransformer","DLinear"]:
+		elif self.FREQ_DOMAIN in ["FITS_Pai","FITS_LPF","FITS","iTransformer","DLinear", "FreTS"]:
 			class Config: pass
 			config = Config()
 
@@ -69,6 +70,8 @@ class MyModel(nn.Module):
 					self.fits_node = iTransformerModel(configs=config)
 				elif self.FREQ_DOMAIN == "DLinear":
 					self.fits_node = DLinearModel(configs=config)
+				elif self.FREQ_DOMAIN == "FreTS":
+					self.fits_node = FreTSModel(configs=config)
 
 				config.enc_in = args['feature_log'] 
 				if self.FREQ_DOMAIN == "FITS":
@@ -81,6 +84,8 @@ class MyModel(nn.Module):
 					self.fits_log = iTransformerModel(configs=config)
 				elif self.FREQ_DOMAIN == "DLinear":	
 					self.fits_log = DLinearModel(configs=config)
+				elif self.FREQ_DOMAIN == "FreTS":
+					self.fits_log = FreTSModel(configs=config)
 
 				config.enc_in = args['feature_edge'] 
 				if self.FREQ_DOMAIN == "FITS":
@@ -93,6 +98,8 @@ class MyModel(nn.Module):
 					self.fits_edge = iTransformerModel(configs=config)
 				elif self.FREQ_DOMAIN == "DLinear":
 					self.fits_edge = DLinearModel(configs=config)
+				elif self.FREQ_DOMAIN == "FreTS":
+					self.fits_edge = FreTSModel(configs=config)
 			else:
 				config.enc_in = 10
 				if self.FREQ_DOMAIN == "FITS":
@@ -105,6 +112,8 @@ class MyModel(nn.Module):
 					self.shared_fits = iTransformerModel(configs=config)
 				elif self.FREQ_DOMAIN == "DLinear":
 					self.shared_fits = DLinearModel(configs=config)
+				elif self.FREQ_DOMAIN == "FreTS":	
+					self.shared_fits = FreTSModel(configs=config)
 				self.modality_proj = nn.ModuleDict({
 					'node': nn.Linear(args['feature_node'], config.enc_in),
 					'log': nn.Linear(args['feature_log'], config.enc_in),
@@ -229,7 +238,7 @@ class MyModel(nn.Module):
 			rec_edge = torch.matmul(rec_edge1.permute(
 				0, 1, 3, 2), self.trace2pod.float()).permute(0, 1, 3, 2)
 			rec = torch.concat([rec_node, rec_log, rec_edge], dim=-1)
-		elif self.FREQ_DOMAIN in ["FITS_Pai","FITS_LPF","FITS","GPT2","iTransformer","DLinear"]:
+		elif self.FREQ_DOMAIN in ["FITS_Pai","FITS_LPF","FITS","GPT2","iTransformer","DLinear","FreTS"]:
 			B, T, _,_ = x['data_node'].shape
 			# get edge mask
 			edge_exists_mask = (self.node_efea.sum(dim=-1) != 0)  # [N, N] boolean mask
