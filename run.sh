56419ece46d65d9d9f8f763c6db919396fdcaded
theamrzaki <https://theamrzaki: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@github.com/theamrzaki>
2025-07-17 16:07:07 -0400

    (FEDformer)

--- run.sh
+++ run.sh
@@ -25,6 +25,7 @@
 #python3 main.py --FREQ_DOMAIN='encoder_decoder' --fewshot_ratio=0.1
 
 #python3 main.py --FREQ_DOMAIN='FITS' --rec_lambda=1.0 --auxi_lambda=0.5
-python3 main.py --FREQ_DOMAIN='TimesNet' --rec_lambda=1.0 --auxi_lambda=0.1
-python3 main.py --FREQ_DOMAIN='FreTS' --rec_lambda=1.0 --auxi_lambda=0.1
+#python3 main.py --FREQ_DOMAIN='TimesNet' --rec_lambda=1.0 --auxi_lambda=0.1
+#python3 main.py --FREQ_DOMAIN='FreTS' --rec_lambda=1.0 --auxi_lambda=0.1
 
+python3 main.py --FREQ_DOMAIN='FEDformerModel' --rec_lambda=1.0 --auxi_lambda=0.1
