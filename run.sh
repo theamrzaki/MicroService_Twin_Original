52f366a28827cacc49f28e8d2b348f1c7ed35df4
theamrzaki <https://theamrzaki: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@github.com/theamrzaki>
2025-07-20 19:09:16 -0400

    (few shots legndre)

--- run.sh
+++ run.sh
@@ -32,4 +32,9 @@
 
 #python3 main.py --FREQ_DOMAIN='FITS_chebyshev' --req_loss_approach='chebyshev-style' --rec_lambda=1.0 --auxi_lambda=0.1
 #python3 main.py --FREQ_DOMAIN='FITS_lag' --req_loss_approach='lag-style' --rec_lambda=1.0 --auxi_lambda=0.1
-python3 main.py --FREQ_DOMAIN='FITS_hermite' --req_loss_approach='hermite-style' --rec_lambda=1.0 --auxi_lambda=0.1
+#python3 main.py --FREQ_DOMAIN='FITS_hermite' --req_loss_approach='hermite-style' --rec_lambda=1.0 --auxi_lambda=0.1
+
+
+python3 main.py --FREQ_DOMAIN='FITS_Legendre' --req_loss_approach='Legendre-style' --fewshot_ratio=0.3
+python3 main.py --FREQ_DOMAIN='FITS_Legendre' --req_loss_approach='Legendre-style' --fewshot_ratio=0.5
+python3 main.py --FREQ_DOMAIN='FITS_Legendre' --req_loss_approach='Legendre-style' --fewshot_ratio=0.7
