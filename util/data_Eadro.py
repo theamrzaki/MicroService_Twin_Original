bec6682d01ac15ad0cba7f93edf847b2cab602c6
theamrzaki <https://theamrzaki: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@github.com/theamrzaki>
2025-12-10 16:51:08 -0500

    (data processing of SN and TT)

--- util/data_Eadro.py
+++ util/data_Eadro.py
@@ -3,7 +3,7 @@ import torch
 
 class chunkDataset(Dataset): #[node_num, T, else]
     def __init__(self, chunks, node_num, edges, window_size):
-        self.data = []
+        self.dataset = []
         self.logs = []
         self.metrics = []
         self.traces = []
@@ -11,6 +11,7 @@ class chunkDataset(Dataset): #[node_num, T, else]
         self.idx2id = {}
         self.window = window_size
         self.percent = 0.5
+        self.first_graph = None
         for idx, chunk_id in enumerate(chunks.keys()):
             self.idx2id[idx] = chunk_id
             chunk = chunks[chunk_id]
@@ -19,8 +20,8 @@ class chunkDataset(Dataset): #[node_num, T, else]
             graph["logs"] = torch.FloatTensor(chunk["logs"])
             graph["metrics"] = torch.FloatTensor(chunk["metrics"])
             graph["traces"] = torch.FloatTensor(chunk["traces"])
-            print(node_num, chunk["logs"].shape, chunk["metrics"].shape, chunk["traces"].shape)
-            self.data.append((graph, chunk["culprit"]))
+            #print(node_num, chunk["logs"].shape, chunk["metrics"].shape, chunk["traces"].shape)
+            #self.data.append((graph, chunk["culprit"]))
             self.logs.append(torch.FloatTensor(chunk["logs"])) #<-- no window dim (why ?), aggregated over all window
             self.metrics.append(torch.FloatTensor(chunk["metrics"]))#
             self.traces.append(torch.FloatTensor(chunk["traces"]))
@@ -29,7 +30,8 @@ class chunkDataset(Dataset): #[node_num, T, else]
                 hot_encode_label[chunk["culprit"]] = 1.0
             chunk["culprit"] = hot_encode_label
             self.losses.append(chunk["culprit"])
-
+            if self.first_graph is None:
+                self.first_graph = graph["adjacency_matrix"]
         #---logs---
         self.logs_array = torch.stack(self.logs)#(B, N,log_dim=1)<-- was aggregated over all window, so now just repeat it
         logs_expanded = np.expand_dims(self.logs_array, axis=1)# Expand (Insert) the window dimension at index 1 --> (B, 1, N, log_dim)
@@ -67,6 +69,25 @@ class chunkDataset(Dataset): #[node_num, T, else]
             self.label_mask[idx, mask] = 2
         self.label_mask = np.eye(3)[self.label_mask.astype(int)]
 
+        self.dataset = []
+        for i in range(self.logs_array.shape[0]):
+            record = {}
+            record['data_log'] = self.logs_array[i] #(window, N, log_dim)
+            record['data_node'] = self.metrics_array[i] #(window, N, metric_dim)
+            record['data_edge'] = self.traces_array[i] #(window, N, N, trace_dim)
+            record['groundtruth_real'] = self.label[i] #(N, 2)
+            record['groundtruth_cls'] = self.label_mask[i] #(N, 3)
+            self.dataset.append(record)
+
+        """
+        processed.dataset[0]["data_node"].shape --> #(10, 5, 3)
+        processed.dataset[0]["data_log"].shape --> #(10, 5, 256)
+        processed.dataset[0]["groundtruth_cls"].shape --> #(5, 3) --> label_mask
+        processed.dataset[0]["groundtruth_real"].shape --> #(5, 2)  --> label
+        processed.dataset[0]["data_edge"].shape --> #(10, 5, 5, 7)
+        """
+
+
     def create_graph(self, edges, node_num):
         # Convert lists to NumPy arrays for easier processing
         source_nodes = np.array(edges[0])
@@ -85,47 +106,47 @@ class chunkDataset(Dataset): #[node_num, T, else]
         return {"adjacency_matrix"  :adjacency_matrix}
 
     def __len__(self):
-        return len(self.data)
+        return len(self.dataset)
     def __getitem__(self, idx):
-        return self.data[idx]
+        return self.dataset[idx]
     def __get_chunk_id__(self, idx):
         return self.idx2id[idx]
 
-from utils_Eadro import *
+from util.utils_Eadro import *
 
-import argparse
-parser = argparse.ArgumentParser()
-parser.add_argument("--random_seed", default=42, type=int)
-
-### Training params
-parser.add_argument("--gpu", default=False, type=lambda x: x.lower() == "true")
-parser.add_argument("--epoches", default=5, type=int)
-parser.add_argument("--batch_size", default=256, type=int)
-parser.add_argument("--lr", default=0.001, type=float)
-parser.add_argument("--patience", default=10, type=int)
-
-##### Fuse params
-parser.add_argument("--self_attn", default=True, type=lambda x: x.lower() == "true")
-parser.add_argument("--fuse_dim", default=128, type=int)
-parser.add_argument("--alpha", default=0.5, type=float)
-parser.add_argument("--locate_hiddens", default=[64], type=int, nargs='+')
-parser.add_argument("--detect_hiddens", default=[64], type=int, nargs='+')
-
-##### Source params
-parser.add_argument("--log_dim", default=16, type=int)
-parser.add_argument("--trace_kernel_sizes", default=[2], type=int, nargs='+')
-parser.add_argument("--trace_hiddens", default=[64], type=int, nargs='+')
-parser.add_argument("--metric_kernel_sizes", default=[2], type=int, nargs='+')
-parser.add_argument("--metric_hiddens", default=[64], type=int, nargs='+')
-parser.add_argument("--graph_hiddens", default=[64], type=int, nargs='+')
-parser.add_argument("--attn_head", default=4, type=int, help="For gat or gat-v2")
-parser.add_argument("--activation", default=0.2, type=float, help="use LeakyReLU, shoule be in (0,1)")
-
-##### Data params
-parser.add_argument("--data", type=str,  default="SN")
-parser.add_argument("--result_dir", default="../result/")
-
-params = vars(parser.parse_args())
+#import argparse
+#parser = argparse.ArgumentParser()
+#parser.add_argument("--random_seed", default=42, type=int)
+#
+#### Training params
+#parser.add_argument("--gpu", default=False, type=lambda x: x.lower() == "true")
+#parser.add_argument("--epoches", default=5, type=int)
+#parser.add_argument("--batch_size", default=256, type=int)
+#parser.add_argument("--lr", default=0.001, type=float)
+#parser.add_argument("--patience", default=10, type=int)
+#
+###### Fuse params
+#parser.add_argument("--self_attn", default=True, type=lambda x: x.lower() == "true")
+#parser.add_argument("--fuse_dim", default=128, type=int)
+#parser.add_argument("--alpha", default=0.5, type=float)
+#parser.add_argument("--locate_hiddens", default=[64], type=int, nargs='+')
+#parser.add_argument("--detect_hiddens", default=[64], type=int, nargs='+')
+#
+###### Source params
+#parser.add_argument("--log_dim", default=16, type=int)
+#parser.add_argument("--trace_kernel_sizes", default=[2], type=int, nargs='+')
+#parser.add_argument("--trace_hiddens", default=[64], type=int, nargs='+')
+#parser.add_argument("--metric_kernel_sizes", default=[2], type=int, nargs='+')
+#parser.add_argument("--metric_hiddens", default=[64], type=int, nargs='+')
+#parser.add_argument("--graph_hiddens", default=[64], type=int, nargs='+')
+#parser.add_argument("--attn_head", default=4, type=int, help="For gat or gat-v2")
+#parser.add_argument("--activation", default=0.2, type=float, help="use LeakyReLU, shoule be in (0,1)")
+#
+###### Data params
+#parser.add_argument("--data", type=str,  default="SN")
+#parser.add_argument("--result_dir", default="../result/")
+#
+#params = vars(parser.parse_args())
 
 import logging
 def get_device(gpu):
@@ -149,39 +170,43 @@ def assert_shapes(shapes, dataset):
     assert dataset.traces_array.shape == (batch, shapes["window_size"], shapes["node_num"], shapes["node_num"], shapes["trace_dim"]), f"Wrong traces shape"
     assert dataset.label_mask.shape == (batch, shapes["node_num"],3), f"Wrong losses shape"
 
-def run(evaluation_epoch=10):
-    data_dir = os.path.join("/home/db2003/Desktop/Amr/Tests/Eadro/codes/chunks", params["data"])
+def run(data="SN"):
+    data_dir = os.path.join("/home/db2003/Desktop/Amr/Tests/Eadro/codes/chunks", data)
 
     metadata = read_json(os.path.join(data_dir, "metadata.json"))
     event_num, node_num, metric_num =  metadata["event_num"], metadata["node_num"], metadata["metric_num"]
     edges = metadata["edges"]
-    params["chunk_lenth"] = metadata["chunk_lenth"]
-
-    hash_id = dump_params(params)
-    params["hash_id"] = hash_id
-    seed_everything(params["random_seed"])
-    device = get_device(params["gpu"])
+    chunk_lenth = 10
 
     train_chunks, test_chunks = load_chunks(data_dir)
 
-    train_data = chunkDataset(train_chunks, node_num, edges,params["chunk_lenth"])
-    test_data = chunkDataset(test_chunks, node_num, edges, params["chunk_lenth"])
+    train_data = chunkDataset(train_chunks, node_num, edges,chunk_lenth)
+    test_data = chunkDataset(test_chunks, node_num, edges, chunk_lenth)
     
     shapes = {
         "metric_dim": metric_num,
         "trace_dim": 2,
-        "window_size": params["chunk_lenth"],
+        "window_size": chunk_lenth,
         "node_num": node_num,
         "event_num": event_num
     }
     assert_shapes(shapes,train_data)
 
 
-    train_dl = DataLoader(train_data, batch_size=params["batch_size"], shuffle=True, collate_fn=collate, pin_memory=True)
-    test_dl = DataLoader(test_data, batch_size=params["batch_size"], shuffle=False, collate_fn=collate, pin_memory=True)
+    #train_dl = DataLoader(train_data, batch_size=params["batch_size"], shuffle=True, collate_fn=collate, pin_memory=True)
+    #test_dl = DataLoader(test_data, batch_size=params["batch_size"], shuffle=False, collate_fn=collate, pin_memory=True)
 
 
-    logging.info("Current hash_id {}".format(hash_id))
-
+    #logging.info("Current hash_id {}".format(hash_id))
+    return train_data, test_data
 if "__main__" == __name__:
     run()
+
+
+"""
+processed.dataset[0]["data_node"].shape --> #(10, 5, 3)
+processed.dataset[0]["data_log"].shape --> #(10, 5, 256)
+processed.dataset[0]["groundtruth_cls"].shape --> #(5, 3) --> label_mask
+processed.dataset[0]["groundtruth_real"].shape --> #(5, 2)  --> label
+processed.dataset[0]["data_edge"].shape --> #(10, 5, 5, 7)
+"""
