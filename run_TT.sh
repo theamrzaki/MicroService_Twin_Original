c452fda8a0d8092d41765daab6559047ceaa14c3
theamrzaki <https://theamrzaki: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@github.com/theamrzaki>
2025-12-22 04:25:21 -0500

    (Anofusion results)

--- run_TT.sh
+++ run_TT.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
 FREQ_DOMAINS=(
- "iTransformer"
-  "FreTS"
-  "DLinear"
-  "encoder_decoder"
-  "TimesNet"
-  "FEDformerModel"
-  "FITS_Legendre"  
+ #"iTransformer"
+ # "FreTS"
+ # "DLinear"
+ # "encoder_decoder"
+ # "TimesNet"
+ # "FEDformerModel"
+ # "FITS_Legendre" OrEdge
+ # "Eadro"  
+ "AnoFusion"
 )
 
-SEEDS=(2 3)
+SEEDS=(1 2 3)
 
 for SEED in "${SEEDS[@]}"; do
     for FREQ in "${FREQ_DOMAINS[@]}"; do
@@ -20,14 +22,36 @@ for SEED in "${SEEDS[@]}"; do
         --FREQ_DOMAIN="$FREQ" \
         --req_loss_approach='Legendre-style' \
         --random_seed="$SEED" \
-        --data_source=SE
+        --data_source=SN
     else
       /bin/python3 main.py \
         --FREQ_DOMAIN="$FREQ" \
         --random_seed="$SEED" \
-        --data_source=SE
+        --data_source=SN
     fi
 
   done
 done
 
+
+
+SEEDS=(1 2 3)
+
+for SEED in "${SEEDS[@]}"; do
+    for FREQ in "${FREQ_DOMAINS[@]}"; do
+  
+    if [ "$FREQ" = "FITS_Legendre" ]; then
+      /bin/python3 main.py \
+        --FREQ_DOMAIN="$FREQ" \
+        --req_loss_approach='Legendre-style' \
+        --random_seed="$SEED" \
+        --data_source=TT
+    else
+      /bin/python3 main.py \
+        --FREQ_DOMAIN="$FREQ" \
+        --random_seed="$SEED" \
+        --data_source=TT
+    fi
+
+  done
+done
