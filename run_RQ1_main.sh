52371f3d627ac611ce676c5a1f9a0c992f2b424a
theamrzaki <https://theamrzaki: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@github.com/theamrzaki>
2026-03-17 17:39:55 -0400

    (running ablations and basis again so as with linear instead of CNN)

--- run_RQ1_main.sh
+++ run_RQ1_main.sh
@@ -20,61 +20,7 @@ FREQ_DOMAINS=(
 
 )
 
-#SEEDS=(1)
-#
-#for SEED in "${SEEDS[@]}"; do
-#    for FREQ in "${FREQ_DOMAINS[@]}"; do
-#  
-#    if [ "$FREQ" = "FITS_Legendre" ]; then
-#      /bin/python3 main.py \
-#        --FREQ_DOMAIN="$FREQ" \
-#        --req_loss_approach='Legendre-style' \
-#        --filter_used="LPF" \
-#        --random_seed="$SEED" \
-#        --data_source=SN \
-#        --experiment_name="RQ1_main" 
-#    else
-#      /bin/python3 main.py \
-#        --FREQ_DOMAIN="$FREQ" \
-#        --req_loss_approach='Normal-Recreation' \
-#        --random_seed="$SEED" \
-#        --data_source=SN \
-#        --experiment_name="RQ1_main"
-#    fi
-#
-#  done
-#done
-#
-#
-#
-#
-#SEEDS=(1)
-#
-#for SEED in "${SEEDS[@]}"; do
-#    for FREQ in "${FREQ_DOMAINS[@]}"; do
-#  
-#    if [ "$FREQ" = "FITS_Legendre" ]; then
-#      /bin/python3 main.py \
-#        --FREQ_DOMAIN="$FREQ" \
-#        --req_loss_approach='Legendre-style' \
-#        --filter_used="LPF" \
-#        --random_seed="$SEED" \
-#        --data_source=MSDS \
-#        --experiment_name="RQ1_main"
-#    else
-#      /bin/python3 main.py \
-#        --FREQ_DOMAIN="$FREQ" \
-#        --req_loss_approach='Normal-Recreation' \
-#        --random_seed="$SEED" \
-#        --data_source=MSDS \
-#        --experiment_name="RQ1_main"
-#    fi
-#
-#  done
-#done
-
-
-SEEDS=(1 2 3)
+SEEDS=(2 3)
 
 for SEED in "${SEEDS[@]}"; do
     for FREQ in "${FREQ_DOMAINS[@]}"; do
@@ -85,19 +31,73 @@ for SEED in "${SEEDS[@]}"; do
         --req_loss_approach='Legendre-style' \
         --filter_used="LPF" \
         --random_seed="$SEED" \
-        --data_source=TT \
+        --data_source=SN \
+        --experiment_name="RQ1_main" 
+    else
+      /bin/python3 main.py \
+        --FREQ_DOMAIN="$FREQ" \
+        --req_loss_approach='Normal-Recreation' \
+        --random_seed="$SEED" \
+        --data_source=SN \
+        --experiment_name="RQ1_main"
+    fi
+
+  done
+done
+
+
+
+
+SEEDS=(2 3)
+
+for SEED in "${SEEDS[@]}"; do
+    for FREQ in "${FREQ_DOMAINS[@]}"; do
+  
+    if [ "$FREQ" = "FITS_Legendre" ]; then
+      /bin/python3 main.py \
+        --FREQ_DOMAIN="$FREQ" \
+        --req_loss_approach='Legendre-style' \
+        --filter_used="LPF" \
+        --random_seed="$SEED" \
+        --data_source=MSDS \
         --experiment_name="RQ1_main"
     else
       /bin/python3 main.py \
         --FREQ_DOMAIN="$FREQ" \
         --req_loss_approach='Normal-Recreation' \
         --random_seed="$SEED" \
-        --data_source=TT \
+        --data_source=MSDS \
         --experiment_name="RQ1_main"
     fi
 
   done
 done
+
+
+#SEEDS=(1 2 3)
+#
+#for SEED in "${SEEDS[@]}"; do
+#    for FREQ in "${FREQ_DOMAINS[@]}"; do
+#  
+#    if [ "$FREQ" = "FITS_Legendre" ]; then
+#      /bin/python3 main.py \
+#        --FREQ_DOMAIN="$FREQ" \
+#        --req_loss_approach='Legendre-style' \
+#        --filter_used="LPF" \
+#        --random_seed="$SEED" \
+#        --data_source=TT \
+#        --experiment_name="RQ1_main"
+#    else
+#      /bin/python3 main.py \
+#        --FREQ_DOMAIN="$FREQ" \
+#        --req_loss_approach='Normal-Recreation' \
+#        --random_seed="$SEED" \
+#        --data_source=TT \
+#        --experiment_name="RQ1_main"
+#    fi
+#
+#  done
+#done
 
 
 
--- run_RQ2_ablations.sh
+++ run_RQ2_ablations.sh
@@ -3,47 +3,47 @@
 FREQ_DOMAIN="FITS_Legendre" 
 data_source="TT"
 
-SEEDS=(1 2 3)
+SEEDS=(1)
 # OrAnomaly / without TexFilter (LPF) / without time-freq fredf loss
 for SEED in "${SEEDS[@]}"; do
 
       # OrAnomaly (ours)
-      #echo "================================================================="
-      #echo "Running ablation: without TexFilter (LPF)"  
-      #echo "================================================================="
-      #/bin/python3 main.py \
-      #  --FREQ_DOMAIN="$FREQ_DOMAIN" \
-      #  --req_loss_approach='Legendre-style' \
-      #  --random_seed="$SEED" \
-      #  --data_source="$data_source" \
-      #  --filter_used="LPF" \
-      #  --experiment_name="RQ2_ablations_components" 
+      echo "================================================================="
+      echo "Running ablation: without TexFilter (LPF)"  
+      echo "================================================================="
+      /bin/python3 main.py \
+        --FREQ_DOMAIN="$FREQ_DOMAIN" \
+        --req_loss_approach='Legendre-style' \
+        --random_seed="$SEED" \
+        --data_source="$data_source" \
+        --filter_used="LPF" \
+        --experiment_name="RQ2_ablations_components" 
+            #
       #
-#
-      ## Ablation: LPF + no linear attention
-      #echo "================================================================="
-      #echo "Running ablation: LPF + no linear attention"
-      #echo "================================================================="
-      #/bin/python3 main.py \
-      #  --FREQ_DOMAIN="$FREQ_DOMAIN" \
-      #  --random_seed="$SEED" \
-      #  --data_source="$data_source" \
-      #  --modules_attn="no_attn" \
-      #  --filter_used="LPF" \
-      #  --experiment_name="RQ2_ablations_components" 
-      #  
-#
-      ## Ablation: without time-freq (remove time dimension lambda) fredf loss
-      #echo "================================================================="
-      #echo "Running ablation: LPF + without time-freq (remove time dimension lambda) fredf loss"
-      #echo "================================================================="
-      #/bin/python3 main.py \
-      #  --FREQ_DOMAIN="$FREQ_DOMAIN" \
-      #  --random_seed="$SEED" \
-      #  --data_source="$data_source" \
-      #  --experiment_name="RQ2_ablations_components" \
-      #  --filter_used="LPF" \
-      #  --rec_lambda=1.0 --auxi_lambda=0.0
+      # Ablation: LPF + no linear attention
+      echo "================================================================="
+      echo "Running ablation: LPF + no linear attention"
+      echo "================================================================="
+      /bin/python3 main.py \
+        --FREQ_DOMAIN="$FREQ_DOMAIN" \
+        --random_seed="$SEED" \
+        --data_source="$data_source" \
+        --modules_attn="no_attn" \
+        --filter_used="LPF" \
+        --experiment_name="RQ2_ablations_components" 
+        
+      #
+      # Ablation: without time-freq (remove time dimension lambda) fredf loss
+      echo "================================================================="
+      echo "Running ablation: LPF + without time-freq (remove time dimension lambda) fredf loss"
+      echo "================================================================="
+      /bin/python3 main.py \
+        --FREQ_DOMAIN="$FREQ_DOMAIN" \
+        --random_seed="$SEED" \
+        --data_source="$data_source" \
+        --experiment_name="RQ2_ablations_components" \
+        --filter_used="LPF" \
+        --rec_lambda=1.0 --auxi_lambda=0.0
 
 
       echo "================================================================="
@@ -55,4 +55,32 @@ for SEED in "${SEEDS[@]}"; do
         --data_source="$data_source" \
         --experiment_name="RQ2_ablations_components" \
         --filter_used="nofilter" 
+done
+
+
+
+#!/bin/bash
+
+FREQ_DOMAIN="FITS_Legendre" 
+req_loss_approach="Legendre-style"
+
+basis_type=("chebyshev" "fourier" "hermite" "laguerre" "legendre" )
+SEEDS=(1)
+#"fourier"
+# Run OrAnomaly with different basis types (both in the projection and FreDF loss)
+for SEED in "${SEEDS[@]}"; do
+      for basis in "${basis_type[@]}"; do
+        echo "================================================================="
+        echo "Running OrAnomaly with basis: $basis"
+        echo "================================================================="
+        /bin/python3 main.py \
+          --FREQ_DOMAIN="$FREQ_DOMAIN" \
+          --req_loss_approach="$req_loss_approach" \
+          --random_seed="$SEED" \
+          --data_source=TT \
+          --basis_type="$basis" \
+          --experiment_name="RQ2_basis_comparison"  \
+          --filter_used="LPF" \
+
+      done
 done
