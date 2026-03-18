5f57c2697698d76524b1c4ab9c7d503d98ec656f
theamrzaki <https://theamrzaki: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@github.com/theamrzaki>
2026-03-18 08:44:07 -0400

    (TT again for the memory, working on seeds 2 and 3 for MSDS and SN)

--- run_RQ1_main.sh
+++ run_RQ1_main.sh
@@ -1,20 +1,69 @@
 #!/bin/bash
 
+
+
 FREQ_DOMAINS=(
- # "Eadro"  
+  "Eadro"  
 
-  "FITS_Legendre" #OrEdge
+  #"FITS_Legendre" #OrEdge
 
-#"AnoFusion"
-#"encoder_decoder"
-#"Art"
+"AnoFusion"
+"encoder_decoder"
+"Art"
+
+
+ "FreTS"
+ "DLinear"
+"iTransformer"
+ "TimesNet"
+  "FEDformerModel"
+
+
+
+)
+
+
+SEEDS=(2 3)
+
+for SEED in "${SEEDS[@]}"; do
+    for FREQ in "${FREQ_DOMAINS[@]}"; do
+  
+    if [ "$FREQ" = "FITS_Legendre" ]; then
+      /bin/python3 main.py \
+        --FREQ_DOMAIN="$FREQ" \
+        --req_loss_approach='Legendre-style' \
+        --filter_used="LPF" \
+        --random_seed="$SEED" \
+        --data_source=MSDS \
+        --experiment_name="RQ1_main"
+    else
+      /bin/python3 main.py \
+        --FREQ_DOMAIN="$FREQ" \
+        --req_loss_approach='Normal-Recreation' \
+        --random_seed="$SEED" \
+        --data_source=MSDS \
+        --experiment_name="RQ1_main"
+    fi
+
+  done
+done
+
+
+FREQ_DOMAINS=(
+  "Eadro"  
+
+  #"FITS_Legendre" #OrEdge
+
+"AnoFusion"
+"encoder_decoder"
+"Art"
 #
 #
-# "FreTS"
-# "DLinear"
-#"iTransformer"
-# "TimesNet"
-#  "FEDformerModel"
+ "FreTS"
+ "DLinear"
+"iTransformer"
+ "TimesNet"
+  "FEDformerModel"
 
 
 
@@ -45,60 +94,32 @@ for SEED in "${SEEDS[@]}"; do
   done
 done
 
-
-
-
-SEEDS=(2 3)
-
-for SEED in "${SEEDS[@]}"; do
-    for FREQ in "${FREQ_DOMAINS[@]}"; do
-  
-    if [ "$FREQ" = "FITS_Legendre" ]; then
-      /bin/python3 main.py \
-        --FREQ_DOMAIN="$FREQ" \
-        --req_loss_approach='Legendre-style' \
-        --filter_used="LPF" \
-        --random_seed="$SEED" \
-        --data_source=MSDS \
-        --experiment_name="RQ1_main"
-    else
-      /bin/python3 main.py \
-        --FREQ_DOMAIN="$FREQ" \
-        --req_loss_approach='Normal-Recreation' \
-        --random_seed="$SEED" \
-        --data_source=MSDS \
-        --experiment_name="RQ1_main"
-    fi
-
-  done
-done
-
-
-SEEDS=(1 2 3)
-
-for SEED in "${SEEDS[@]}"; do
-    for FREQ in "${FREQ_DOMAINS[@]}"; do
-  
-    if [ "$FREQ" = "FITS_Legendre" ]; then
-      /bin/python3 main.py \
-        --FREQ_DOMAIN="$FREQ" \
-        --req_loss_approach='Legendre-style' \
-        --filter_used="LPF" \
-        --random_seed="$SEED" \
-        --data_source=TT \
-        --experiment_name="RQ1_main"
-    else
-      /bin/python3 main.py \
-        --FREQ_DOMAIN="$FREQ" \
-        --req_loss_approach='Normal-Recreation' \
-        --random_seed="$SEED" \
-        --data_source=TT \
-        --experiment_name="RQ1_main"
-    fi
-
-  done
-done
-
-
-
-
+#SEEDS=(1 2 3)
+#
+#for SEED in "${SEEDS[@]}"; do
+#    for FREQ in "${FREQ_DOMAINS[@]}"; do
+#  
+#    if [ "$FREQ" = "FITS_Legendre" ]; then
+#      /bin/python3 main.py \
+#        --FREQ_DOMAIN="$FREQ" \
+#        --req_loss_approach='Legendre-style' \
+#        --filter_used="LPF" \
+#        --random_seed="$SEED" \
+#        --data_source=TT \
+#        --experiment_name="RQ1_main"
+#    else
+#      /bin/python3 main.py \
+#        --FREQ_DOMAIN="$FREQ" \
+#        --req_loss_approach='Normal-Recreation' \
+#        --random_seed="$SEED" \
+#        --data_source=TT \
+#        --experiment_name="RQ1_main"
+#    fi
+#
+#  done
+#done
+#
+#
+#
+#
+#
