677de69a4193f596c39a61f4d2e5b17f54e2e430
theamrzaki <https://theamrzaki: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@github.com/theamrzaki>
2026-03-23 12:25:34 -0400

    (backbones of TT with FreDF)

--- run_RQ1_main.sh
+++ run_RQ1_main.sh
@@ -4,13 +4,13 @@
 
 FREQ_DOMAINS=(
 
-  "Eadro"  
-#
-"FITS_Legendre" #OrEdge
-#
-"AnoFusion"
-"encoder_decoder"
-"Art"
+#  "Eadro"  
+##
+#"FITS_Legendre" #OrEdge
+##
+#"AnoFusion"
+#"encoder_decoder"
+#"Art"
 ##
 ##
  "FreTS"
@@ -50,6 +50,33 @@ ORANOMALY_MODELS=("FITS_Legendre" "FreTS" "DLinear" "iTransformer" "FEDformerMod
 #done
 
 
+#SEEDS=(1 2 3)
+#
+#for SEED in "${SEEDS[@]}"; do
+#    for FREQ in "${FREQ_DOMAINS[@]}"; do
+#  
+#    if [[ " ${ORANOMALY_MODELS[@]} " =~ " ${FREQ} " ]]; then
+#      /bin/python3 main.py \
+#        --FREQ_DOMAIN="$FREQ" \
+#        --req_loss_approach='Legendre-style' \
+#        --filter_used="LPF" \
+#        --random_seed="$SEED" \
+#        --data_source=SN \
+#        --experiment_name="RQ1_main" 
+#    else
+#      /bin/python3 main.py \
+#        --FREQ_DOMAIN="$FREQ" \
+#        --req_loss_approach='Normal-Recreation' \
+#        --random_seed="$SEED" \
+#        --data_source=SN \
+#        --experiment_name="RQ1_main"
+#    fi
+#
+#  done
+#done
+
+
+
 SEEDS=(1 2 3)
 
 for SEED in "${SEEDS[@]}"; do
@@ -61,46 +88,19 @@ for SEED in "${SEEDS[@]}"; do
         --req_loss_approach='Legendre-style' \
         --filter_used="LPF" \
         --random_seed="$SEED" \
-        --data_source=SN \
-        --experiment_name="RQ1_main" 
+        --data_source=TT \
+        --experiment_name="RQ1_main"
     else
       /bin/python3 main.py \
         --FREQ_DOMAIN="$FREQ" \
         --req_loss_approach='Normal-Recreation' \
         --random_seed="$SEED" \
-        --data_source=SN \
+        --data_source=TT \
         --experiment_name="RQ1_main"
     fi
 
   done
 done
-
-
-
-#SEEDS=(1)
-#
-#for SEED in "${SEEDS[@]}"; do
-#    for FREQ in "${FREQ_DOMAINS[@]}"; do
-#  
-#    if [ "$FREQ" = "FITS_Legendre" ]; then
-#      /bin/python3 main.py \
-#        --FREQ_DOMAIN="$FREQ" \
-#        --req_loss_approach='Legendre-style' \
-#        --filter_used="LPF" \
-#        --random_seed="$SEED" \
-#        --data_source=TT \
-#        --experiment_name="RQ1_main"
-#    else
-#      /bin/python3 main.py \
-#        --FREQ_DOMAIN="$FREQ" \
-#        --req_loss_approach='Normal-Recreation' \
-#        --random_seed="$SEED" \
-#        --data_source=TT \
-#        --experiment_name="RQ1_main"
-#    fi
-#
-#  done
-#done
 #
 #
 #
