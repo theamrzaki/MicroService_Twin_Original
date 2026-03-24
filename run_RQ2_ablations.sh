dd1148924621cf952ee0f47b1d964bb6b6c5e11f
theamrzaki <https://theamrzaki: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@github.com/theamrzaki>
2026-03-24 13:02:23 -0400

    (ablations and basis more seeds)

--- run_RQ2_ablations.sh
+++ run_RQ2_ablations.sh
@@ -3,58 +3,58 @@
 FREQ_DOMAIN="FITS_Legendre" 
 data_source="TT"
 
-SEEDS=(1)
+SEEDS=(2 3)
 # OrAnomaly / without LPF / without time-freq fredf loss
 for SEED in "${SEEDS[@]}"; do
 
-      ## OrAnomaly (ours)
-      #echo "================================================================="
-      #echo "Running ablation: without LPF (with linear attention, with FreDF loss)"  
-      #echo "================================================================="
-      #/bin/python3 main.py \
-      #  --FREQ_DOMAIN="$FREQ_DOMAIN" \
-      #  --req_loss_approach='Legendre-style' \
-      #  --random_seed="$SEED" \
-      #  --data_source="$data_source" \
-      #  --filter_used="LPF" \
-      #  --experiment_name="RQ2_ablations_components" 
-      #      #
-      ##
-      ## Ablation: LPF + no linear attention
-      #echo "================================================================="
-      #echo "Running ablation: no linear attention (with LPF, with FreDF loss)"
-      #echo "================================================================="
-      #/bin/python3 main.py \
-      #  --FREQ_DOMAIN="$FREQ_DOMAIN" \
-      #  --random_seed="$SEED" \
-      #  --data_source="$data_source" \
-      #  --modules_attn="no_attn" \
-      #  --filter_used="LPF" \
-      #  --experiment_name="RQ2_ablations_components" 
-      #  
-      ##
-      ## Ablation: without time-freq (remove time dimension lambda) fredf loss
-      #echo "================================================================="
-      #echo "Running ablation: without FreDF [remove time dimension lambda] (with linear attention and LPF)"
-      #echo "================================================================="
-      #/bin/python3 main.py \
-      #  --FREQ_DOMAIN="$FREQ_DOMAIN" \
-      #  --random_seed="$SEED" \
-      #  --data_source="$data_source" \
-      #  --experiment_name="RQ2_ablations_components" \
-      #  --filter_used="LPF" \
-      #  --rec_lambda=1.0 --auxi_lambda=0.0
-#
-#
-      #echo "================================================================="
-      #echo "Running ablation: No filter applied (with linear attention, with FreDF loss)"
-      #echo "================================================================="
-      #/bin/python3 main.py \
-      #  --FREQ_DOMAIN="$FREQ_DOMAIN" \
-      #  --random_seed="$SEED" \
-      #  --data_source="$data_source" \
-      #  --experiment_name="RQ2_ablations_components" \
-      #  --filter_used="nofilter" 
+      # OrAnomaly (ours)
+      echo "================================================================="
+      echo "Running ablation: without LPF (with linear attention, with FreDF loss)"  
+      echo "================================================================="
+      /bin/python3 main.py \
+        --FREQ_DOMAIN="$FREQ_DOMAIN" \
+        --req_loss_approach='Legendre-style' \
+        --random_seed="$SEED" \
+        --data_source="$data_source" \
+        --filter_used="LPF" \
+        --experiment_name="RQ2_ablations_components" 
+            #
+      #
+      # Ablation: LPF + no linear attention
+      echo "================================================================="
+      echo "Running ablation: no linear attention (with LPF, with FreDF loss)"
+      echo "================================================================="
+      /bin/python3 main.py \
+        --FREQ_DOMAIN="$FREQ_DOMAIN" \
+        --random_seed="$SEED" \
+        --data_source="$data_source" \
+        --modules_attn="no_attn" \
+        --filter_used="LPF" \
+        --experiment_name="RQ2_ablations_components" 
+
+      #
+      # Ablation: without time-freq (remove time dimension lambda) fredf loss
+      echo "================================================================="
+      echo "Running ablation: without FreDF [remove time dimension lambda] (with linear attention and LPF)"
+      echo "================================================================="
+      /bin/python3 main.py \
+        --FREQ_DOMAIN="$FREQ_DOMAIN" \
+        --random_seed="$SEED" \
+        --data_source="$data_source" \
+        --experiment_name="RQ2_ablations_components" \
+        --filter_used="LPF" \
+        --rec_lambda=1.0 --auxi_lambda=0.0
+      #
+      #
+      echo "================================================================="
+      echo "Running ablation: No filter applied (with linear attention, with FreDF loss)"
+      echo "================================================================="
+      /bin/python3 main.py \
+        --FREQ_DOMAIN="$FREQ_DOMAIN" \
+        --random_seed="$SEED" \
+        --data_source="$data_source" \
+        --experiment_name="RQ2_ablations_components" \
+        --filter_used="nofilter" 
 
 
       echo "================================================================="
@@ -73,26 +73,26 @@ done
 
 #!/bin/bash
 
-#FREQ_DOMAIN="FITS_Legendre" 
-#req_loss_approach="Legendre-style"
-#
-#basis_type=("chebyshev" "fourier" "hermite" "laguerre" "legendre" )
-#SEEDS=(1)
-##"fourier"
-## Run OrAnomaly with different basis types (both in the projection and FreDF loss)
-#for SEED in "${SEEDS[@]}"; do
-#      for basis in "${basis_type[@]}"; do
-#        echo "================================================================="
-#        echo "Running OrAnomaly with basis: $basis"
-#        echo "================================================================="
-#        /bin/python3 main.py \
-#          --FREQ_DOMAIN="$FREQ_DOMAIN" \
-#          --req_loss_approach="$req_loss_approach" \
-#          --random_seed="$SEED" \
-#          --data_source=TT \
-#          --basis_type="$basis" \
-#          --experiment_name="RQ2_basis_comparison"  \
-#          --filter_used="LPF" \
-#
-#      done
-#done
+FREQ_DOMAIN="FITS_Legendre" 
+req_loss_approach="Legendre-style"
+
+basis_type=("chebyshev" "fourier" "hermite" "laguerre" "legendre" )
+SEEDS=(2 3)
+#"fourier"
+# Run OrAnomaly with different basis types (both in the projection and FreDF loss)
+for SEED in "${SEEDS[@]}"; do
+      for basis in "${basis_type[@]}"; do
+        echo "================================================================="
+        echo "Running OrAnomaly with basis: $basis"
+        echo "================================================================="
+        /bin/python3 main.py \
+          --FREQ_DOMAIN="$FREQ_DOMAIN" \
+          --req_loss_approach="$req_loss_approach" \
+          --random_seed="$SEED" \
+          --data_source=TT \
+          --basis_type="$basis" \
+          --experiment_name="RQ2_basis_comparison"  \
+          --filter_used="LPF" \
+
+      done
+done
