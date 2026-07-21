68178540adc4fad34b4c9bb36f87871e77f98912
theamrzaki <theamrzaki@hotmail.com>
2026-07-21 17:54:10 -0400

    (seeds 2 and 3) expect for Art

--- run_RQ1_main_inference.sh
+++ run_RQ1_main_inference.sh
@@ -1,33 +1,22 @@
 #!/bin/bash
 
-
-
 FREQ_DOMAINS=(
+"FITS_Legendre" #OrEdge
 ##
-#"FITS_Legendre" #OrEdge
-##
-#"AnoFusion"
-#"Eadro"  
+"AnoFusion"
+"Eadro"  
 "encoder_decoder"
-#"Art"
-##
-##
-# "FreTS"
-# "DLinear"
-#"iTransformer"
- #"TimesNet"
- # "FEDformerModel"
-
-
+"Art"
 )
+
 ORANOMALY_MODELS=("FITS_Legendre" "FreTS" "DLinear" "iTransformer" "FEDformerModel" "FITS_LENGDRE_parallel_oth_compoenents")
-DATA_SOURCES=("SN" "MSDS" "TT") # ) MSDS 
+DATA_SOURCES=("SN" "MSDS" "TT")  
 DEVICE_NAME="Raspberry Pi Smaller"
 # as miniforge is installed on Raspberry Pi
 source ~/miniforge3/etc/profile.d/conda.sh
 conda activate RCAEval
 
-SEEDS=(1)
+SEEDS=(2 3)
 
 for SEED in "${SEEDS[@]}"; do
   for datasource in "${DATA_SOURCES[@]}"; do
@@ -37,6 +26,12 @@ for SEED in "${SEEDS[@]}"; do
       EPOCHS=50
     else
       EPOCHS=300
+    fi
+
+    #skip encoder_decoder on TT as it is not working (would run and then stops at the end)
+    if [[ "$datasource" == "TT" && "$FREQ" == "encoder_decoder" ]]; then
+      echo "Skipping $FREQ on $datasource as it is not working"
+      continue
     fi
 
     if [[ " ${ORANOMALY_MODELS[@]} " =~ " ${FREQ} " ]]; then
@@ -74,5 +69,71 @@ for SEED in "${SEEDS[@]}"; do
 done
 
 
+
+
+
+FREQ_DOMAINS=(
+"Art"
+
+
+)
+ORANOMALY_MODELS=("FITS_Legendre" "FreTS" "DLinear" "iTransformer" "FEDformerModel" "FITS_LENGDRE_parallel_oth_compoenents")
+DATA_SOURCES=("SN" "MSDS" "TT") # ) MSDS 
+DEVICE_NAME="Raspberry Pi Smaller"
+# as miniforge is installed on Raspberry Pi
+source ~/miniforge3/etc/profile.d/conda.sh
+conda activate RCAEval
+
+SEEDS=(1)
+
+for SEED in "${SEEDS[@]}"; do
+  for datasource in "${DATA_SOURCES[@]}"; do
+    for FREQ in "${FREQ_DOMAINS[@]}"; do
+    #if TT --> epochs 50 else epochs 300
+    if [[ "$datasource" == "TT" ]]; then
+      EPOCHS=50
+    else
+      EPOCHS=300
+    fi
+
+    #skip encoder_decoder on TT as it is not working (would run and then stops at the end)
+    if [[ "$datasource" == "TT" && "$FREQ" == "encoder_decoder" ]]; then
+      echo "Skipping $FREQ on $datasource as it is not working"
+      continue
+    fi
+
+    if [[ " ${ORANOMALY_MODELS[@]} " =~ " ${FREQ} " ]]; then
+      echo "--------------------------------"
+      echo "1) Running $FREQ with Legendre-style loss and LPF filter, seed $SEED"
+      echo "--------------------------------"
+      python main.py \
+        --FREQ_DOMAIN="$FREQ" \
+        --req_loss_approach='Legendre-style' \
+        --filter_used="LPF" \
+        --random_seed="$SEED" \
+        --data_source="$datasource" \
+        --evaluate=true \
+        --gpu=false\
+        --DEVICE_NAME="$DEVICE_NAME" \
+        --model_path="./result/${FREQ}-${datasource}-Epochs${EPOCHS}-Seed${SEED}ExpNameRQ1_main-lowshow-simplegraph" \
+        --experiment_name="RQ1_main_Edge"
+    else
+      echo "--------------------------------"
+      echo "2) Running $FREQ with Normal-Recreation loss, seed $SEED"
+      echo "--------------------------------"
+      python main.py \
+        --FREQ_DOMAIN="$FREQ" \
+        --req_loss_approach='Normal-Recreation' \
+        --random_seed="$SEED" \
+        --data_source="$datasource" \
+        --gpu=false \
+        --evaluate=true \
+        --DEVICE_NAME="$DEVICE_NAME" \
+        --model_path="./result/${FREQ}-${datasource}-Epochs${EPOCHS}-Seed${SEED}ExpNameRQ1_main-lowshow-simplegraph" \
+        --experiment_name="RQ1_main_Edge"
+    fi
+  done
+  done
+done
 
 
