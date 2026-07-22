8f68ce0e5de41a8c92fe5b62101ea4115591d001
theamrzaki <https://theamrzaki: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@github.com/theamrzaki>
2026-07-22 15:30:39 -0400

    (running MSDS more seeds)

--- copy.sh
+++ copy.sh
@@ -1,13 +1,13 @@
 
 
 #!/bin/bash
-MODEL_NAMES=("Eadro" "AnoFusion" "encoder_decoder" "FITS_Legendre" "Art")
-DATASET_NAMES=("TT") #"MSDS" "SN" 
-#IP_ADDRESS="130.63.254.140" #db2003smaller
-IP_ADDRESS="130.63.103.80"
-SEEDS=("3")
-#DEVICE_NAME="db2003smaller"
-DEVICE_NAME="db2003larger"
+MODEL_NAMES=("Art") #"Eadro" "AnoFusion" "encoder_decoder" "FITS_Legendre" 
+DATASET_NAMES=("TT" "MSDS" "SN" ) #
+IP_ADDRESS="130.63.252.147" #db2003smaller
+#IP_ADDRESS="130.63.103.80"
+SEEDS=(1 2 3)
+DEVICE_NAME="db2003smaller"
+#DEVICE_NAME="db2003larger"
 
 # Give your user permission to read the source folder so you don't need sudo inside the loops
 sudo chown -R $(whoami) "/home/db2003/Desktop/Amr/(Journal) MicroService_Twin_Original/result/"
