775b3948cd60871331eb1bae2d381e1ee8ab911c
theamrzaki <https://theamrzaki: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@github.com/theamrzaki>
2026-09-14 05:36:14 -0400

    (TT runs for basis degrees)

--- run_RQ2z5_basis_degree.sh
+++ run_RQ2z5_basis_degree.sh
@@ -1,11 +1,14 @@
 
 FREQ_DOMAIN="FITS_Legendre"
 req_loss_approach="Legendre-style"
-DATA_SOURCES=("MSDS") # "MSDS" "SN" "TT" 
+DATA_SOURCES=("TT") # "MSDS" "SN" "TT" 
 
-basis_type=("hermite" "laguerre" "legendre" "chebyshev") # ) #"fourier"
-degree=("3" "5" "7" "9")
-SEEDS=(2 3)
+basis_type=("laguerre" "legendre" "chebyshev" ) # ) #"fourier" "hermite" 
+degree=("3" "7" "9")
+SEEDS=(1 2 3)
+
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate RCAEval
 
 for SEED in "${SEEDS[@]}"; do
     for data in "${DATA_SOURCES[@]}"; do
