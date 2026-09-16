f74b615194a6517718c6755eae94f36cba152e8d
theamrzaki <theamrzaki@hotmail.com>
2026-09-15 21:10:59 -0400

    (DeepHunt runs)

--- run_RQ1_main_inference.sh
+++ run_RQ1_main_inference.sh
@@ -11,13 +11,13 @@ FREQ_DOMAINS=(
 )
 
 ORANOMALY_MODELS=("FITS_Legendre" "FreTS" "DLinear" "iTransformer" "FEDformerModel" "FITS_LENGDRE_parallel_oth_compoenents")
-DATA_SOURCES=("SN" "MSDS" "TT")  
+DATA_SOURCES=("TT")  #"SN" "MSDS" 
 DEVICE_NAME="Raspberry Pi Smaller"
 # as miniforge is installed on Raspberry Pi
 source ~/miniforge3/etc/profile.d/conda.sh
 conda activate RCAEval
 
-SEEDS=(3)
+SEEDS=(2)
 
 for SEED in "${SEEDS[@]}"; do
   for datasource in "${DATA_SOURCES[@]}"; do
